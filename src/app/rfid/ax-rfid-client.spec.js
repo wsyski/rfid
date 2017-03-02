956c3d7731b786dd783983cce0f7477187687559
wsyski <wos@axiell.com>
2017-03-02 09:45:47 +0100

    Initial version

--- src/app/rfid/ax-rfid-client.spec.js
+++ src/app/rfid/ax-rfid-client.spec.js
@@ -1,10 +1,13 @@
 describe('RFID Client', function () {
     var READER = "1";
-    var TAG_RESPONSES = [
-        '{"cmd":"tag","id":"id0","type":"Item","reason":"Firsttime new complete","reader": "' + READER + '"}',
-        '{"cmd":"tag","id":"id1","type":"Item","reason":"Firsttime new complete","reader": "' + READER + '"}',
-        '{"cmd":"tag","id":"id2","type":"Item","reason":"Firsttime new complete","reader": "' + READER + '"}'
-    ];
+
+    function cmdTagComplete(id) {
+        return JSON.stringify({"cmd": "tag", "id": id, "type": "Item", "reason": "Firsttime new complete", "reader": READER});
+    }
+
+    function cmdTagPartial(id) {
+        return JSON.stringify({"cmd": "tag", "id": id, "type": "Item", "reason": "Partial", "reader": READER});
+    }
 
     describe('AxRfid.Client', function () {
         var axRfidClient;
@@ -19,7 +22,7 @@ describe('RFID Client', function () {
                 openObserver.onNext();
                 return mockWebSocket;
             });
-            axRfidTagStore=axRfidClient.getTagStore();
+            axRfidTagStore = axRfidClient.getTagStore();
         });
 
         afterEach(function () {
@@ -29,24 +32,39 @@ describe('RFID Client', function () {
             }
         });
 
-        it('tags', function () {
-            var expectedState = Object.assign({},
-                AxRfid.INITIAL_STATE,
-                {isConnected: true, isReady: true},
-                {tags: [new AxRfid.Tag("id0", READER, true), new AxRfid.Tag("id1", READER, true), new AxRfid.Tag("id2", READER, true)]});
-            var states = [];
-            subscription = axRfidTagStore.subscribe(function (data) {
+        it('cmd tag', function () {
+            function test(expectedState,cmds) {
+                var states = [];
+                subscription = axRfidTagStore.subscribe(function (data) {
                     states.push(data);
+                });
+                axRfidClient.connect("name");
+                cmds.forEach(function (json) {
+                    var e = {};
+                    e.data = json;
+                    mockWebSocket.onNext(e);
+                });
+                var lastState = states[states.length - 1];
+                expect(lastState).toEqual(expectedState);
+            }
+
+            it('all complete', function () {
+                var expectedState = Object.assign({},
+                    AxRfid.INITIAL_STATE,
+                    {isConnected: true, isReady: true},
+                    {tags: [new AxRfid.Tag("id0", READER, true), new AxRfid.Tag("id1", READER, true), new AxRfid.Tag("id2", READER, true)]});
+                var cmds = [cmdTagComplete("id0"), cmdTagComplete("id1"), cmdTagComplete("id2")];
+                test(expectedState,cmds);
             });
-            axRfidClient.connect("name");
-            TAG_RESPONSES.forEach(function (json) {
-                var e = {};
-                e.data = json;
-                mockWebSocket.onNext(e);
+
+            it('1 partial and 2 complete', function () {
+                var expectedState = Object.assign({},
+                    AxRfid.INITIAL_STATE,
+                    {isConnected: true, isReady: true},
+                    {tags: [new AxRfid.Tag("id0", READER, false), new AxRfid.Tag("id1", READER, true), new AxRfid.Tag("id2", READER, true)]});
+                var cmds = [cmdTagPartial("id0"), cmdTagComplete("id1"), cmdTagComplete("id2")];
+                test(expectedState,cmds);
             });
-            var lastState = states[states.length - 1];
-            expect(lastState).toEqual(expectedState);
         });
     });
-})
-;
+});
